7bc1c19e7a899f7be66ec821206f32c8f3fd4a60
Zeicks <sheremet666666@gmail.com>
2022-10-06 14:15:31 +0300

    add_plugins

--- cypress/e2e/Tests/Temple_and_Webster/HomeDecor.cy.js
+++ cypress/e2e/Tests/Temple_and_Webster/HomeDecor.cy.js
@@ -1,6 +1,9 @@
 describe('Test_Temple_Webster_HomeDecor', () => {
     it('Test 1 item', () => {
       cy.visit('https://www.templeandwebster.com.au/Blush-French-Linen-European-Pillowcase-79458-TMPL1585.html?refid=Renovai447-TMPL1585');
+      cy.request('https://www.templeandwebster.com.au/Blush-French-Linen-European-Pillowcase-79458-TMPL1585.html?refid=Renovai447-TMPL1585').then( ({ status }) => {
+        expect(status).to.eq(200)
+      });
 
       cy.scrollTo(0, 1100);
      if  (cy.get('#tpw_renovai_frame').should('be.visible')){

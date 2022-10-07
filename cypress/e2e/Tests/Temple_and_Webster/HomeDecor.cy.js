038f9bd542f66ee5e3d2ca1d90fd88361370a27b
Zeicks <sheremet666666@gmail.com>
2022-10-07 17:04:43 +0300

    1

--- cypress/e2e/Tests/Temple_and_Webster/HomeDecor.cy.js
+++ cypress/e2e/Tests/Temple_and_Webster/HomeDecor.cy.js
@@ -1,16 +1,21 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
 describe('Test_Temple_Webster_HomeDecor', () => {
-    it('Test 1 item', () => {
-      cy.visit('https://www.templeandwebster.com.au/Admiral-Cocky-Acrylic-Wall-Art-ICON1297.html?refid=Renovai447-ICON1297_200519255&PiID%5B%5D=200519255');
+  it('Test 1 item', () => {
+    cy.visit('https://www.templeandwebster.com.au/Admiral-Cocky-Acrylic-Wall-Art-ICON1297.html?refid=Renovai447-ICON1297_200519255&PiID%5B%5D=200519255');
 
-      cy.request('https://www.templeandwebster.com.au/Admiral-Cocky-Acrylic-Wall-Art-ICON1297.html?refid=Renovai447-ICON1297_200519255&PiID%5B%5D=200519255')
-      .then( ({ status }) => {
+    cy.request('https://www.templeandwebster.com.au/Admiral-Cocky-Acrylic-Wall-Art-ICON1297.html?refid=Renovai447-ICON1297_200519255&PiID%5B%5D=200519255')
+      .then(({ status }) => {
         expect(status).to.eq(200)
       });
 
-      cy.scrollTo(0, 1100);
-     if  (cy.get('#tpw_renovai_frame', {timeout:2000}).should('be.visible')){
-        cy.log('Test sucessfful')
-     } else (cy.log('Cant create scene Test fail'));
-    })
-
+    cy.scrollTo(0, 1100);
+    if (cy.get('#tpw_renovai_frame', { timeout: 2000 }).should('be.visible')) {
+      cy.log('Test sucessfful')
+    } else (cy.log('Cant create scene Test fail'));
   })
+
+})

cc53b669449837bbf47fed8536bd03837dfb26d5
Zeicks <sheremet666666@gmail.com>
2022-10-21 14:34:08 +0300

    add_test_TnW1

--- cypress/e2e/Tests/Temple_and_Webster/TnW_1.cy.js
+++ cypress/e2e/Tests/Temple_and_Webster/TnW_1.cy.js
@@ -1,43 +1,45 @@
-import 'cypress-iframe';
-import 'cypress-xpath'
+import { scene } from '../../PageObject/Temple_and_Webster/scene';
+import { links } from '../../PageObject/Temple_and_Webster/links';
+import { network } from '../../PageObject/Temple_and_Webster/network';
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
+  return false
 })
 
 describe('Temple and Webster MB', () => {
-  //  const token = "'body > app-root:nth-child(3) > app-shell:nth-child(2) > div:nth-child(1) > perfect-scrollbar:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > app-final:nth-child(2) > app-mode-default:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > app-scene-mb:nth-child(1) > div:nth-child(1) > div:nth-child(2)'";
-
-    it('Test coverlet set', () => {
-        cy.visit('https://www.templeandwebster.com.au/Queens-Cotton-Coverlet-Set-CLQT1102.html?refid=Renovai447-CLQT1102_200536374&PiID%5B%5D=200536374');
-        cy.scrollTo(0, 1000);
-
-        cy.frameLoaded('#renovai-frame-0');
-        cy.wait(8000);
-        cy.iframe('#renovai-frame-0')
-            .xpath('//div[@class="mb-item ng-star-inserted"][1]', { timeout: 3000 }).click()
-            .xpath('//i[@class = "info__x font-icon-exit ng-star-inserted"]').click()
-            .xpath('//app-alt-card[2]').click();
-
-        cy.iframe('#renovai-frame-0')
-            .contains('SEE SIMILAR').click()
-            .xpath('//app-alt-card[1]').click();
-       
-        cy.iframe('#renovai-frame-0')
-        .contains('ADD TO CART').click({force: true});
-
-        cy.xpath('//div[@class= "js-basket-dropdown-container"]//a//span[@class= "top-nav-icon"]').click();
-        
 
 
+  before('Get Sku And Visit Site', () => {
+    cy.intercept('/projects/2', (req) => {
+      req.continue((res) => {
+        expect(res.body).to.include('My Project')
+      })
+    });
+    cy.intercept('GET', network.apiRequest).as('request');
+    cy.intercept('POST', network.createSkuUserInfo).as('sku');
+    cy.visit(links.TnW_1);
+    cy.wait('@request').its(network.haveBody).should('eq', true);
+    cy.wait('@sku').then((interception) => {
+      cy.log(interception.response.body.sku);
+    });
+  });
 
+  // Scroll to ifream ,load and waite ifream
+  it('Change Item On Scene', () => {
+    scene.iframeLoad();
+    cy.iframe(scene.iframe)
+      .xpath(scene.itemOnScene, { timeout: 3000 }).click()
+      .xpath(scene.closeComponent).click()
+      .xpath(scene.alternativeSecondItemInBottom).click();
+  });
 
+  it('Change Item From Sidebar', () => {
+    cy.iframe(scene.iframe)
+      .contains(scene.seeSimilar, { timeout: 1000 }).click()
+      .xpath(scene.alternativeFirstItemInBottom).click();
+  });
 
-        // cy.iframe('#renovai-frame-0').get(token, { timeout: 3000 }).click();
-        //
-
-        //cy.iframe('.mb-item.ng-star-inserted.__web-inspector-hide-shortcut__').click();
-        // xpath for BA //div[@class="mb-item ng-star-inserted"][2]
-    })
-})
+  it('Add To Cart From Sidebar ', () => {
+    cy.iframe(scene.iframe)
+      .contains(scene.addToCart).click({ force: true });
+  });
+});
